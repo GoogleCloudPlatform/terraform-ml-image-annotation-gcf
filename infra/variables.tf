e7cc9d32bb71bc87b93dfedae81243a8ee13e9cd
xsxm <121201207+xsxm@users.noreply.github.com>
2023-06-27 16:55:32 -0700

    fix: change var.gcf_location to region (#24)

--- infra/variables.tf
+++ infra/variables.tf
@@ -29,10 +29,10 @@ variable "time_to_enable_apis" {
   default     = "420s"
 }
 
-variable "gcf_location" {
+variable "region" {
   description = "GCF deployment location/region."
   type        = string
-  default     = "us-west4" #us-central1
+  default     = "us-west4"
 }
 
 variable "gcf_max_instance_count" {
